789ab2b0a561aefb9909c3694a8fab876e7f469e
Opilane TTHK <opilane@tthk.ee>
2020-12-02 11:22:32 +0200

    Марки машин

---
+++ masterAutod/masterAutod/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace masterAutod
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            MainPhoto.Source = ImageSource.FromFile("mainFoto.jpg");
+            carList.ItemsSource = GetMenuItems();
+            var homepage = typeof(View.buggati);
+            Detail = new NavigationPage((Page)Activator.CreateInstance(homepage));
+            IsPresented = false;
+        }
+
+        public List<MasterMenuItems> GetMenuItems()
+        {
+            var list = new List<MasterMenuItems>();
+
+            list.Add(new MasterMenuItems()
+            {
+                Text = "Bugatti",
+                Detail = "Все о Bugatti",
+                ImagePath = "buggatiLogo.png",
+                TargetPage = typeof(View.buggati)
+            });
+            list.Add(new MasterMenuItems()
+            {
+                Text = "Alfa Romeo",
+                Detail = "Все о Alfa Romeo",
+                ImagePath = "alfaromeoLogo.png",
+                TargetPage = typeof(View.alfaRomeo)
+            });
+            list.Add(new MasterMenuItems()
+            {
+                Text = "Dodge",
+                Detail = "Все о Dodge",
+                ImagePath = "dodgeLogo.png",
+                TargetPage = typeof(View.dodge)
+            });
+            list.Add(new MasterMenuItems()
+            {
+                Text = "Chevrolet",
+                Detail = "Все о Chevrolet",
+                ImagePath = "chevroletLogo.png",
+                TargetPage = typeof(View.chevrolet)
+            });
+            list.Add(new MasterMenuItems()
+            {
+                Text = "Ford",
+                Detail = "Все о Ford",
+                ImagePath = "fordLogo.png",
+                TargetPage = typeof(View.ford)
+            });
+
+            return list;
+        }
+
+        private void carList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selected = (MasterMenuItems)e.SelectedItem;
+            Type selectedpage = selected.TargetPage;
+            Detail = new NavigationPage((Page)Activator.CreateInstance(selectedpage));
+            IsPresented = false;
+        }
+    }
+}
---
+++ masterAutod/masterAutod/View/chevrolet.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace masterAutod.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class chevrolet : ContentPage
+    {
+        public chevrolet()
+        {
+            InitializeComponent();
+        }
+
+        private void modelChevrolet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
